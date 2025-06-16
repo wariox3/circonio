c838fe5e46192984d1b7ef860a2a51c4aea63d01
sebastian <sebastian.h.piedrahita@gmail.com>
2025-06-16 09:29:42 -0500

    fix: importacion de carpeta redux y ahora se llama store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,7 +9,7 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { EffectsApp, StoreApp } from './redux';
+import { EffectsApp, StoreApp } from './store';
 import { tokenInterceptor } from './common/interceptors/token.interceptor';
 import { httpErrorInterceptor } from './common/interceptors/error-handler/http-error.interceptor';
 
