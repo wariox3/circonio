03d46be9dd729ae611b8b677599ce7501e2d8112
sebastian <sebastian.h.piedrahita@gmail.com>
2025-06-17 10:24:53 -0500

    fix: las keys no coinciden con reddoc

--- src/app/core/constants/localstorage-keys.constant.ts
+++ src/app/core/constants/localstorage-keys.constant.ts
@@ -1,6 +1,6 @@
 export const LOCALSTORAGE_KEYS = {
-  AUTH_TOKEN: 'auth-token',
-  REFRESH_TOKEN: 'refresh-token',
-  USER: 'user',
+  AUTH_TOKEN: 'token',
+  REFRESH_TOKEN: 'RefreshToken',
+  USER: 'usuario',
   SUBDOMAIN: 'subdomain',
 };
